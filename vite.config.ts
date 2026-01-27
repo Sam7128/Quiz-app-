ee44c01ec6a47c46f8e7e6f81b10e12aa58686d2
Sam7128 <keroro7128@gmail.com>
2026-01-27 20:36:49 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
     minify: 'esbuild', // 將 'terser' 改為 'esbuild'，或直接刪除這一行
