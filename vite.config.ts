533bd13a3e98baff0c6f07d5b4e2dd9308ccb052
Sam7128 <keroro7128@gmail.com>
2026-01-27 14:39:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // Build output directory
-    minify: 'terser', // Enable minification, can also use 'esbuild'
+    outDir: 'dist',
+    minify: 'esbuild', // 將 'terser' 改為 'esbuild'，或直接刪除這一行
     rollupOptions: {
-      // Additional Rollup options can be configured here
       output: {
-        // Custom output options
         entryFileNames: '[name].[hash].js',
         chunkFileNames: '[name].[hash].js',
         assetFileNames: '[name].[hash][ext]',
